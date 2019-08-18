c2f2668c91beee7e3edfa4912c10376270ed7577
sami suliman <sami.bco@gmail.com>
2019-08-18 12:46:57 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,10 +1,11 @@
-def hello_t(names)
-  names.each do |name|
+def hello_t(array)
+
+   array.each do |name|
     if name.start_with?("T")
-      puts "Hi, #{name}"
+        puts "Hi, #{name}"
     end
-  end 
-end
+  end
+end	
 
 # call your method here!
 
